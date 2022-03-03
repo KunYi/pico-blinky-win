aa9326b87e181893f825298b7eda1d01ad801f0c
KunYi Chen/陳坤毅 <kunyi.chen@gmail.com>
2022-03-03 13:04:18 +0800

    import version

---
+++ envsetup.ps1
@@ -0,0 +1,4 @@
+$Env:PICO_SDK_PATH="C:\Users\kunic\source\repos\pico\pico-sdk"
+$Env:PICO_TOOLCHAIN_PATH="C:\Program Files (x86)\GNU Arm Embedded Toolchain\10 2021.10"
+Import-Module 'C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\Tools\Microsoft.VisualStudio.DevShell.dll'
+Enter-VsDevShell b7a71a89
